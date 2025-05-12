d537cc1643b43ef24eb68e21e79c390180373409
toji14 <darwinpomaguayo@gmail.com>
2025-05-11 23:44:48 -0500

    Update Main.py

--- APP/Main.py
+++ APP/Main.py
@@ -1,26 +1,26 @@
-"""devuelve un mensaje"""
-
+"""Juego de adivinanzas."""
 
 import random
-num_azar = random.randint(1,100)
+
+num_azar = random.randint(1, 100)
 num_guess = 0
 tries = 0
+
+
 def juego_adivinanza(num):
-  
-  if num_guess > num_azar:
-        print("El numero es mayor, vuelva a intentarlo")
-  elif num_guess < num_azar:
-        print("El numero es menor, vuelva a intentarlo")
-         
-    
+    """Proporciona una pista al jugador."""
+    if num_guess > num_azar:
+        print("El número es mayor, vuelva a intentarlo")
+    elif num_guess < num_azar:
+        print("El número es menor, vuelva a intentarlo")
 
 
 while True:
-    num_guess= int(input("Ingrese un valor(1-100):"))
+    num_guess = int(input("Ingrese un valor (1-100): "))
     if num_guess == num_azar:
         break
-    
+
     juego_adivinanza(num_guess)
-    tries +=1
-    
-print(f"Felicidades, adivinaste el numero {num_guess} en {tries} intentos")
+    tries += 1
+
+print(f"Felicidades, adivinaste el número {num_guess} en {tries} intentos")
